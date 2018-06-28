1dc35a34deb27e2c5c6e6ddf3cda742488e329d3
saefulrizky <saefulrizky2@gmail.com>
2018-06-28 10:30:45 +0700

    prima selesai

---
+++ c4/prima.js
@@ -0,0 +1,25 @@
+function cekPrima(n){
+  var max = n/2;
+  for (var i = 2; i <= max; i++) {
+    if(n % i == 0)
+    return false;
+  }
+  return true;
+}
+
+function indexPrima(param1){
+  var prim = [];
+  var number = 3;
+  if(param1 > 1) prim.push(1);
+  while(prim.length != param1){
+    if(cekPrima(number)){
+      prim.push(number);
+    }
+    number++;
+  }
+  return prim[prim.length - 1];
+}
+
+console.log(indexPrima(4));
+console.log(indexPrima(500));
+console.log(indexPrima(37786));
