047dac7dc3950944acf6bda66ca4a45a40a418dd
saefulrizky <saefulrizky2@gmail.com>
2018-07-02 14:30:17 +0700

    readline selesai

---
+++ c10/readline.js
@@ -0,0 +1,31 @@
+function stringManipulation(word){
+  if(word.charAt(0).match(/[aeiouAEIOU]/)){
+    return word;
+  }else{
+    var newWord = word.slice(1,word.length).concat(word.charAt(0)).concat('nyo');
+    return newWord;
+  }
+}
+
+function sentenceManipulation(sentence){
+  var arraySentence = sentence.split(" ");
+  for (var i = 0; i < arraySentence.length; i++) {
+    arraySentence[i] = stringManipulation(arraySentence[i]);
+  }
+  var newSentence = arraySentence.toString().replace(/,/g , " ");
+  return newSentence;
+}
+
+function readline(){
+  var readline = require('readline');
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output : process.stdout
+  });
+  rl.question('Tulis kalimatmu di sini > ', (answer) => {
+    console.log("hasil konversi: "+ sentenceManipulation(`${answer}`));
+    rl.close();
+  });
+}
+
+readline();
