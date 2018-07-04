0535c09743813bf1d5ae01fbdbeb7903e5aef9e9
saefulrizky <saefulrizky2@gmail.com>
2018-07-04 13:30:31 +0700

    tebak kata 2 challenge 12 selesai

--- c12/solution.js
+++ c12/solution.js
@@ -5,36 +5,59 @@ function getJson(file){
   return data;
 }
 
-function solution(json){
-  console.log("Selamat datang di permainan Tebak-tebakan, kamu akan diberikan pertanyaan dari file ini "+json+".");
-  console.log("Untuk bermain, jawablah dengan jawaban yang sesuai.");
-  console.log("Gunakan skip 'skip' untuk menangguhkan pertanyaanya, dan di akhir pertanyaan akan ditanyakan lagi.\n");
+function tebakKata(json){
   var data = getJson(json);
   var readline = require('readline');
-  var i = 0;
+  var wrong = 1;
   var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: 'Jawaban: '
+    prompt: 'Tebakan:'
   });
-  console.log("Pertanyaan: "+ data[i].definition);
+
+  console.log(`Selamat datang di permainan Tebak-tebakan, kamu akan diberikan pertanyaan dari file ini ${json}.`);
+  console.log("Untuk bermain, jawablah dengan jawaban yang sesuai.");
+  console.log("Gunakan skip 'skip' untuk menangguhkan pertanyaanya, dan di akhir pertanyaan akan ditanyakan lagi.\n");
+  console.log(`Pertayaan: ${data[0].definition}`);
   rl.prompt();
+
   rl.on('line', (input) =>{
-    if(i < data.length){
-      if(data[i].term == `${input}`){
-        console.log("\nAnda Beruntung!\n");
-        i++;
-        if(i < data.length)
-          console.log("Pertanyaan: "+ data[i].definition);
+    if(data[0].term == `${input}`){
+      console.log("Anda Beruntung!");
+      data.shift();
+      if(0 < data.length){
+        console.log(`\nPertayaan: ${data[0].definition}`);
+        rl.prompt();
       }else{
-        console.log("\nAnda Kurang Beruntung! anda telah salah 1 kali, silahkan coba lagi.");
+        console.log("\nHore Anda Berhasil!");
+        rl.close();
       }
+      wrong = 1;
+    }else if(`${input}` == "skip"){
+      let temp = data[0];
+      data.shift();
+      data.push(temp);
+      console.log(`\nPertayaan: ${data[0].definition}`);
       rl.prompt();
     }else{
-      rl.close();
-      console.log("\nAnda Berhasil!");
+        console.log("\nAnda kurang beruntung! anda telah salah "+wrong+" kali, silahkan coba lagi.\n");
+        wrong++;
+        rl.prompt();
     }
-  })
+  });
 }
 
-solution('science.json');
+function solution(){
+  let json = null;
+  process.argv.forEach((val, index) => {
+    json = val;
+  });
+  if(json.slice(json.length-5, json.length) == ".json"){
+      tebakKata(json);
+  }else{
+    console.log("Tolong sertakan nama file dengan inputan soalnya");
+    process.exit(0);
+  }
+}
+
+solution();
